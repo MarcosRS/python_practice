b436de27ea0fd4529b6bdfe3f7a244076d3c1896
Marcos Reynoso <marcos.reynoso@dowjones.com>
2019-06-04 19:17:00 -0400

    start: file handling

---
+++ files.py
@@ -0,0 +1,20 @@
+# files paths , file names, file extensions
+# os module contains file path fuction (compatible for windows, mac, linux)
+import os
+
+#generates path for current os
+print(os.path.join('folder1', 'folder2', 'folder3'))
+
+os.sep #\\
+
+print(os.getcwd()) # get current working directory
+
+#to change directory
+os.chdir('/')
+
+# . root folder
+#.. parent folder
+
+
+
+
