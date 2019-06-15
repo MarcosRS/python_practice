7406594b23aaac72e9f973d43f03504d301a0cfa
MarcosRS <MarcosRS16@gmail.com>
2019-06-15 09:54:25 -0400

    add Files

--- files.py
+++ files.py
@@ -98,3 +98,58 @@ shelveFile['cats'] # should have ['zophie', 'pooka', 'simon']
 
 
 
+
+
+
+# Cyping and Moving
+import shutil
+shutil.copy('./orgin/text.txt', './destination') # copies text.txt to the destination folder
+shutil.copy('./orgin/text.txt', './destination/text_2.txt') # copies text.txt to the destination folder and renames it
+
+# Copy entire folder
+shutil.copytree('./orginFolder', './destinationFolder') # copies entire directory
+
+# Move file
+shutil.move('./orgin/text.txt', './destination') # moves file
+
+# You can use move comman to rename files since shutil does not an integrated method for this
+shutil.move('./orgin/text.txt', './orgin/text_2.txt') # moves file
+
+
+
+
+
+# Deleting  (3 ways to do it)
+#1 
+import os
+os.unlink('bacon.txt') # will delete permanently
+os.rmdir('./folder') #will only delete folder if it's empty 
+
+#2
+import shutil
+shutil.rmtree() #removes entire folder and everthything in it (permanent delete)
+
+#instructor recommend console loggin before deleting anything
+
+#3 use send2trash (needs intalation - pip)
+import send2trash 
+send2trash.send2trash('./folder') #does not delete it , it simply sends it to the recycling bin 
+
+
+
+
+# walking directory
+import os
+for folderName, subfolders, files in os.walk('./folder')
+    print('The folder is' + folderName)
+    print('The subfolders in ' + folderName + ' are: ' + str(subfolders))
+    print('The filenames in ' + folderName + ' are: ' + str(files))
+    print('\n')
+
+    for subfolder in subfolders:
+            if 'fish' in subfolder:
+                os.rmdir(subfolder) #just an example
+    
+    for file in filenames:
+        if file.endswith('.py'):
+            shutil.copy(os.join(foldername, file), file + '.backup')
